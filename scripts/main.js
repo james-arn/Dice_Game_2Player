65bd24c39f326c349ccbc3c89c2026a5d56d9089
James Arnold <j1mesu@hotmail.co.uk>
2021-10-08 15:45:12 +0100

    game fully functional. Moving on to styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,77 +1,73 @@
-//define varables
+//efine global varables
 const newGame = document.getElementById("newGame");
 const p1Text = document.getElementById("p1Text");
+const p2Text = document.getElementById("p2Text");
 let p1TotalScore = document.getElementById("p1TotalScore");
-let p1CurrentScore = document.getElementById("p1CurrentScore")
 let p2TotalScore = document.getElementById("p2TotalScore");
-let p2CurrentScore = document.getElementById("p2CurrentScore")
+let p1CurrentScore = document.getElementById("p1CurrentScore");
+let p2CurrentScore = document.getElementById("p2CurrentScore");
 let diceImage = document.getElementById("diceImage");
 let rollButton = document.getElementById("rollButton");
 let holdButton = document.getElementById("holdButton");
-
-// Set up a running total for both to display in total score
+// These track game score turn and sum
 let p1Sum = 0;
 let p2Sum = 0;
+let isPlayer1 = true;
 
-//MAIN FUNCTIONALITY - Assign a random value to one roll, conditional statement
-//inc. running total & changing image on dice.
+//ROLL - points system & winning conditions
+rollButton.addEventListener("click", respondRollButton);
 
-rollButton.addEventListener("click", respondRollButton)
-
-function respondRollButton () {
+function respondRollButton() {
   let oneRoll = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-  switch (oneRoll) {
-    case 1:
-      p1Sum += 1;
-      diceImage.src = "img/Dice 1.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
-    case 2:
-      p1Sum += 2;
-      diceImage.src = "img/Dice 2.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
-    case 3:
-      p1Sum += 3;
-      diceImage.src = "img/Dice 3.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
-    case 4:
-      p1Sum += 4;
-      diceImage.src = "img/Dice 4.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
-    case 5:
-      p1Sum += 5;
-      diceImage.src = "img/Dice 5.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
-    case 6:
-      p1Sum += 6;
-      diceImage.src = "img/Dice 6.svg";
-      p1CurrentScore.innerText = p1Sum;
-      break;
+  diceImage.src = `img/Dice ${oneRoll}.svg`;
+  if (isPlayer1 == true) {
+    p1Sum += oneRoll;
+    p1CurrentScore.innerText = p1Sum;
+  } else {
+    p2Sum += oneRoll;
+    p2CurrentScore.innerText = p2Sum;
   }
-
-// hold button - doesn't work
-holdButton.addEventListener("click", respondHoldButton)
-
-function respondHoldButton () { 
-  p1Sum 
-  p1CurrentScore.innerText = p1TotalScore.innerText;
-  p1CurrentScore.innerText = 0
+  // within ROLL function: player 1 win conditons:
+  if (p1Sum > 20) {
+    p1Text.innerText = "Player 1 Wins!";
+    p2Text.innerText = "Player 2 Loses!";
+    rollButton.style.display = "none";
+    holdButton.style.display = "none";
+    newGame.style.display = "block";
+  } else if (oneRoll == 1 && isPlayer1 == true) {
+    p1Text.innerText = "Player 1 Loses!";
+    p2Text.innerText = "Player 2 Wins!";
+    rollButton.style.display = "none";
+    holdButton.style.display = "none";
+    newGame.style.display = "block";
+  }
+// within ROLL funcition: player 2 win conditions:
+  if (p2Sum > 20) {
+    p1Text.innerText = "Player 1 Loses!";
+    p2Text.innerText = "Player 2 Wins!";
+    rollButton.style.display = "none";
+    holdButton.style.display = "none";
+    newGame.style.display = "block";
+  } else if (oneRoll == 1 && isPlayer1 == false) {
+    p1Text.innerText = "Player 1 Wins!";
+    p2Text.innerText = "Player 2 Loses!";
+    rollButton.style.display = "none";
+    holdButton.style.display = "none";
+    newGame.style.display = "block";
+  }
 }
 
-  // within funciton doing game win/lose conditions:
-  if (p1Sum > 20) {
-    p1Text.innerText = "Winner!";
-    rollButton.style.display = "none";
-    newGame.style.display = "block";
-  } else if (oneRoll == 1) {
-    p1Text.innerText = "You Lose!";
-    rollButton.style.display = "none";
-    newGame.style.display = "block";
-  }
-};
+ // HOLD functionality
+ holdButton.addEventListener("click", respondHoldButton);
 
-
+ function respondHoldButton() {
+   if (isPlayer1 == true) {
+     p1TotalScore.innerText = p1Sum;
+     p1CurrentScore.innerText = 0;
+     isPlayer1 = false;
+   } else {
+     p2TotalScore.innerText = p2Sum;
+     p2CurrentScore.innerText = 0;
+     isPlayer1 = true;
+   }
+ }
